bebfb9bd25564437d2f05f409bab2fcd13dd1f1f
dxvo <dxvo@uh.edu>
2019-03-28 16:43:55 -0500

    update

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from. flask import Flask
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+#################################################
+# Database Setup
+#################################################
+
+app.config["CLEARDB_DATABASE_URL"] = "mysql://b240f7d100648c:db4a4797@us-cdbr-iron-east-03.cleardb.net/heroku_76070ac0daeb317?reconnect=true"
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# # Save references to each table
+# Samples_Metadata = Base.classes.sample_metadata
+# Samples = Base.classes.samples
+
 
 @app.route("/")
 def index():
     """Return the homepage."""
-    return 'Hello'
+    return 'Hello World'
 
 
 if __name__ == "__main__":
